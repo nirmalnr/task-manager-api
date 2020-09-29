2d7a6f6967f51304e300253ba70d0b1bcbf00dbf
Nirmal <nirmalmetallica@gmail.com>
2020-09-29 20:45:39 +0530

    email unique value uncommented

--- src/models/user.js
+++ src/models/user.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        //unique: true,
+        unique: true,
         trim: true,
         required: true,
         lowercase: true,
